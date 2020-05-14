41b14fef7547d0d7cdd2b250ee3599848a9ac3c7
Eliascm17 <Elias.cmoreno17@gmail.com>
2020-05-14 17:16:09 -0500

    started to integrate swagger api to project

--- BooksApi/Startup.cs
+++ BooksApi/Startup.cs
@@ -14,8 +14,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using BooksApi.Models;
 using BooksApi.Services;
+using Microsoft.OpenApi.Models;
 using Microsoft.CodeAnalysis.Options;
-
+ 
 namespace BooksApi
 {
     public class Startup
@@ -40,11 +41,25 @@ namespace BooksApi
 
             services.AddControllers()
                 .AddNewtonsoftJson(options => options.UseMemberCasing());
+
+            services.AddSwaggerGen(c =>
+           {
+               c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+           {
+               c.SwaggerEndpoint("/Swagger/v1/swagger.json", "My API v1");
+               c.RoutePrefix = string.Empty;
+           });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
